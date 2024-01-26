0dd9118273aa3aeb7850fc7ea298703ca8cbfa78
Priyambada Das <priyambada268@gmail.com>
2024-01-26 19:59:40 +0530

    added service section and appointment section

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,13 +1,21 @@
 const header = document.querySelector("header");
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.scrollY > 60)
 })
 
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
-menu.onclick= () =>{
+menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle("open");
 }
+
+// Show scroll up
+const scrollUp = () => {
+    const scrollUp = document.getElementById("scroll-up")
+    // when the scroll is higher than 350 viewport height, and the show-scroll class to
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp);
